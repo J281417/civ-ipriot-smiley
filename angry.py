19de8f3a128f876d691865b9eea1bb3cb68e91c6
J281417 <J281417@tafe.wa.edu.au>
2025-05-25 21:08:17 +0800

    Complet portfolio activity (#1)
    Co-authored-by: noland254 <noland254@hotmail.com>

--- angry.py
+++ angry.py
@@ -2,9 +2,9 @@ import time
 from blinkable import Blinkable
 from smiley import Smiley
 
-class Sad(Smiley,Blinkable):
+class Angry(Smiley,Blinkable):
     def __init__(self):
-        super().__init__(complexion=self.BLUE)
+        super().__init__(complexion=self.RED)
 
         self.draw_mouth()
         self.draw_eyes()
